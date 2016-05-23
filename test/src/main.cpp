fa970413fd36f16262f52ba2de1594194dd2634b
dianhenglau <dianhenglau@gmail.com>
2016-05-23 21:25:53 +0800

    Add a test case.

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -4,14 +4,16 @@
 // Please implement them in other file.
 void test1(void);
 void test2(void);
-void testcircuit(void);
+int test_circuit(int argc, const char* argv[]);
+void test_get_token(void);
 
 int main(int argc, const char* argv[]) {
     // Call test function here.
     // Comment out those you don't want to test.
     test1();
     test2();
-    testcircuit();
+    //testcircuit(arc, argv);
+    test_get_token();
 
     return 0;
 }
