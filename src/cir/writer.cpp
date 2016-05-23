7b28fe6ab10818f5f5d5650942555a8cc6a67b28
dianhenglau <dianhenglau@gmail.com>
2016-05-23 21:24:53 +0800

    TODO.

--- src/cir/writer.cpp
+++ src/cir/writer.cpp
@@ -1,8 +1,9 @@
-#include "util/writer.h"
+#include "cir/writer.h"
 
-int Util::Writer::write(const std::vector<Cir::Path>&     paths,
-                        const std::vector<Cir::InputVec>& input_vecs,
-                        const std::string&                outfile) {
-	return 0;
+int Cir::write(const std::vector<Cir::Path>&     paths,
+               const std::vector<Cir::InputVec>& input_vecs,
+               const std::string&                outfile)   {
+
+    return 0;
     // TODO
 }
