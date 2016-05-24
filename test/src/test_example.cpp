f5997c3e762ecc6bd8166a4753db1b3252ff4b08
dianhenglau <dianhenglau@gmail.com>
2016-05-24 20:43:22 +0800

    Use __FUNCTION__ to replace function name.

--- test/src/test_example.cpp
+++ test/src/test_example.cpp
@@ -4,7 +4,7 @@
 #include "test/src/util/myrand.h"
 
 void test1(void) {
-    std::cerr << "test1():\n";
+    std::cerr << __FUNCTION__ << "():\n";
 
     // Will I get 3 consecutive same number from myrand()?
     double a = myrand();
@@ -14,11 +14,11 @@ void test1(void) {
     assert(!(a == b && b == c) && 
         "Error: a = b = c, seems like myrand() have some problem");
 
-    std::cerr << "test1() passed.\n";
+    std::cerr << __FUNCTION__ << "() passed.\n";
 }
 
 void test2(void) {
-    std::cerr << "test2():\n";
+    std::cerr << __FUNCTION__ << "():\n";
 
     // Swap without using temporary variable.
     int a = 13, b = 30;
@@ -29,5 +29,5 @@ void test2(void) {
     assert(a == 30 && "Error: a != 30, swap failed.");
     assert(b == 13 && "Error: b != 13, swap failed.");
 
-    std::cerr << "test2() passed.\n";
+    std::cerr << __FUNCTION__ << "() passed.\n";
 }
