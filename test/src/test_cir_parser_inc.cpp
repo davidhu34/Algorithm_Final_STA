dcad95efafdf94047f038fd11d8269514d4b350f
dianhenglau <dianhenglau@gmail.com>
2016-05-24 20:42:42 +0800

    Add test code.

--- test/src/test_cir_parser_inc.cpp
+++ test/src/test_cir_parser_inc.cpp
@@ -1,12 +1,13 @@
-#include <cassert>
+#include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <string>
 
+#include "test/src/util/util.h"
 #include "cir/parser.inc"
 
 void test_get_token(void) {
-    std::cerr << "test_get_token():\n";
+    std::cerr << __FUNCTION__ << "():\n";
 
     std::istringstream sin(
         "haha.hoho, :;(what,who$1,sum_m&er\n)[/]=`*//hello\n*>//");
@@ -25,7 +26,9 @@ void test_get_token(void) {
         out1 += " ";
     }
 
-    assert(out1 == ans1 && "Error: out1 != ans1\n");
+    ASSERT(out1 == ans1,
+        << "\nout1: \"" << TestUtil::escape(out1) << "\""
+        << "\nans1: \"" << TestUtil::escape(ans1) << "\"");
 
     sin.clear();
     sin.seekg(0);
@@ -35,7 +38,158 @@ void test_get_token(void) {
         out2 += " ";
     }
 
-    assert(out2 == ans2 && "Error: out2 != ans2\n");
+    ASSERT(out2 == ans2,
+        << "\nout2 = \"" << TestUtil::escape(out2) << "\""
+        << "\nans2 = \"" << TestUtil::escape(ans2) << "\"");
 
-    std::cerr << "test_get_token() passed.\n";
+    std::cerr << __FUNCTION__ << "() passed.\n";
 }
+
+void test_parse_module_NAND2(void) {
+    std::cerr << __FUNCTION__ << "():\n";
+
+    std::istringstream sin(
+        "NAND2 (Y, A, B);\n"
+        "output Y;\n"
+        "input A, B;\n"
+        "\n"
+        "  nand (Y, A, B);\n"
+        "\n"
+        "  specify\n"
+        "    \n"
+        "    specparam\n"
+        "      tplh$A$Y = 1,\n"
+        "      tphl$A$Y = 1,\n"
+        "      tplh$B$Y = 1,\n"
+        "      tphl$B$Y = 1;\n"
+        "\n"
+        "    \n"
+        "    (A *> Y) = (tplh$A$Y, tphl$A$Y);\n"
+        "    (B *> Y) = (tplh$B$Y, tphl$B$Y);\n"
+        "  endspecify\n"
+        "\n"
+        "endmodule \n"
+        "`endcelldefine\n");
+
+    Cir::Circuit circuit;
+
+    ASSERT(parse_module_NAND2(sin, circuit) == 0, );
+
+    ASSERT(circuit.module_NAND2.type == Cir::Module::NAND2,
+        << "circuit.module_NAND2.type = " << circuit.module_NAND2.type);
+           
+    ASSERT(circuit.module_NAND2.input_names.size() == 2,
+        << "circuit.module_NAND2.input_names.size() = "
+        << circuit.module_NAND2.input_names.size());
+
+    ASSERT(circuit.module_NAND2.input_names[0] == "A",
+        << "circuit.module_NAND2.input_names[0] = "
+        << circuit.module_NAND2.input_names[0]);
+
+    ASSERT(circuit.module_NAND2.input_names[1] == "B",
+        << "circuit.module_NAND2.input_names[1] = "
+        << circuit.module_NAND2.input_names[1]);
+
+    ASSERT(circuit.module_NAND2.output_name == "Y",
+        << "circuit.module_NAND2.output_name = "
+        << circuit.module_NAND2.output_name);
+
+    std::cerr << __FUNCTION__ << "() passed.\n";
+}
+
+void test_parse_module_NOR2(void) {
+    std::cerr << __FUNCTION__ << "():\n";
+
+    std::istringstream sin(
+        "NOR2 (Y, A, B);\n"
+        "output Y;\n"
+        "input A, B;\n"
+        "\n"
+        "  nor (Y, A, B);\n"
+        "\n"
+        "  specify\n"
+        "    \n"
+        "    specparam\n"
+        "      tplh$A$Y = 1,\n"
+        "      tphl$A$Y = 1,\n"
+        "      tplh$B$Y = 1,\n"
+        "      tphl$B$Y = 1;\n"
+        "\n"
+        "    \n"
+        "    (A *> Y) = (tplh$A$Y, tphl$A$Y);\n"
+        "    (B *> Y) = (tplh$B$Y, tphl$B$Y);\n"
+        "  endspecify\n"
+        "\n"
+        "endmodule \n"
+        "`endcelldefine\n");
+
+    Cir::Circuit circuit;
+
+    ASSERT(parse_module_NOR2(sin, circuit) == 0, );
+
+    ASSERT(circuit.module_NOR2.type == Cir::Module::NOR2,
+        << "circuit.module_NOR2.type = " << circuit.module_NOR2.type);
+           
+    ASSERT(circuit.module_NOR2.input_names.size() == 2,
+        << "circuit.module_NOR2.input_names.size() = "
+        << circuit.module_NOR2.input_names.size());
+
+    ASSERT(circuit.module_NOR2.input_names[0] == "A",
+        << "circuit.module_NOR2.input_names[0] = "
+        << circuit.module_NOR2.input_names[0]);
+
+    ASSERT(circuit.module_NOR2.input_names[1] == "B",
+        << "circuit.module_NOR2.input_names[1] = "
+        << circuit.module_NOR2.input_names[1]);
+
+    ASSERT(circuit.module_NOR2.output_name == "Y",
+        << "circuit.module_NOR2.output_name = "
+        << circuit.module_NOR2.output_name);
+
+    std::cerr << __FUNCTION__ << "() passed.\n";
+}
+
+void test_parse_module_NOT1(void) {
+    std::cerr << __FUNCTION__ << "():\n";
+
+    std::istringstream sin(
+        "module NOT1 (Y, A);\n"
+        "output Y;\n"
+        "input A;\n"
+        "\n"
+        "  not I0(Y, A);\n"
+        "  specify\n"
+        "    \n"
+        "    specparam\n"
+        "      tplh$A$Y = 1,\n"
+        "      tphl$A$Y = 1;\n"
+        "\n"
+        "    \n"
+        "    (A *> Y) = (tplh$A$Y, tphl$A$Y);\n"
+        "  endspecify\n"
+        "\n"
+        "endmodule \n"
+        "`endcelldefine\n");
+
+    Cir::Circuit circuit;
+
+    ASSERT(parse_module_NOT1(sin, circuit) == 0, );
+
+    ASSERT(circuit.module_NOT1.type == Cir::Module::NOT1,
+        << "circuit.module_NOT1.type = " << circuit.module_NOT1.type);
+           
+    ASSERT(circuit.module_NOT1.input_names.size() == 1,
+        << "circuit.module_NOT1.input_names.size() = "
+        << circuit.module_NOT1.input_names.size());
+
+    ASSERT(circuit.module_NOT1.input_names[0] == "A",
+        << "circuit.module_NOT1.input_names[0] = "
+        << circuit.module_NOT1.input_names[0]);
+
+    ASSERT(circuit.module_NOT1.output_name == "Y",
+        << "circuit.module_NOT1.output_name = "
+        << circuit.module_NOT1.output_name);
+
+    std::cerr << __FUNCTION__ << "() passed.\n";
+}
+
