8f213ad19f17d0c266f0da014b65c235a192c9a0
dianhenglau <dianhenglau@gmail.com>
2016-05-24 09:11:25 +0800

    TODO

---
+++ src/ana/analyzer.h
@@ -0,0 +1,31 @@
+#ifndef STA_ANA_ANALYZER_H
+#define STA_ANA_ANALYZER_H
+
+#include "cir/circuit.h"
+
+namespace Ana {
+
+// Find sensitizable paths.
+//
+// #### Input
+//
+// - `circuit`: TODO: Should it modify circuit?
+//
+// #### Output
+//
+// - `paths`     : Found sensitizable paths.
+// - `input_vecs`: Corresponding input vector that cause a path become
+//                 true path.
+// - Return an error code: 0 means successful; 1 means error encountered.
+//
+int find_sensitizable_paths(Cir::Circuit&               circuit,
+                            std::vector<Cir::Path>&     paths,
+                            std::vector<Cir::InputVec>& input_vecs) {
+
+    // TODO
+    return 0;
+}
+
+} // namespace Ana
+
+#endif // STA_ANA_ANALYZER_H
