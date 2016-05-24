567444e69eebb4a85e7becce2db44fcc2882543e
dianhenglau <dianhenglau@gmail.com>
2016-05-24 20:42:19 +0800

    Add test.

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -2,18 +2,36 @@
 
 // Put test function prototype here.
 // Please implement them in other file.
+
+// test_example.cpp
 void test1(void);
 void test2(void);
+
+// test_cir_circuit.cpp
 int test_circuit(int argc, const char* argv[]);
+
+// test_cir_parser_inc.cpp
 void test_get_token(void);
+void test_parse_module_NAND2(void);
+void test_parse_module_NOR2(void);
+void test_parse_module_NOT1(void);
 
 int main(int argc, const char* argv[]) {
     // Call test function here.
     // Comment out those you don't want to test.
+    
+    std::cerr << "test_example.cpp\n";
     test1();
     test2();
-    //testcircuit(arc, argv);
+
+    //std::cerr << "\ntest_cir_circuit.cpp";
+    //test_circuit(argc, argv);
+
+    std::cerr << "\ntest_cir_parser_inc.cpp\n";
     test_get_token();
+    test_parse_module_NAND2();
+    test_parse_module_NOR2();
+    test_parse_module_NOT1();
 
     return 0;
 }
