961128365bd93b92724b5b061cadf1bff7a119ab
dianhenglau <dianhenglau@gmail.com>
2016-05-25 17:45:31 +0800

    Test hasher.h.

---
+++ test/src/test_util_hasher.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <string>
+
+#include "test/src/util/util.h"
+
+#include "util/hasher.h"
+
+// Read in a file that contain many words. Take hash value of each of
+// these words. Save these hash value into a table. Use a multimap to
+// map word's hash value to itself. Print out each collision. If number
+// of collision > certain critical value, this test is failed.
+//
+void test_hash_str(void) {
+    std::cerr << __FUNCTION__ << "():\n";
+
+    const char* filename = "test/cases/words.txt";
+
+    std::ifstream fin(filename);
+    ASSERT(fin.good(),
+        << "Cannot open file \"" << filename);
+
+    typedef std::multimap<std::uint32_t, std::string>::value_type     Pair;
+    typedef std::multimap<std::uint32_t, std::string>::const_iterator Iterc;
+
+    std::multimap<std::uint32_t, std::string> dict;
+    std::vector<std::uint32_t>                vec;
+
+    // Read each word and put them into `vec` and `dict`.
+    std::string str;
+    while (fin >> str) {
+        std::uint32_t n = Util::hash_str(str);
+        dict.insert((Pair(n, str)));
+        vec.push_back(n);
+    }
+
+    // Calculate and print collitions.
+    std::cerr << "Collisions:\n";
+
+    int max_num_in_bucket = 0;
+
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (dict.count(vec[i]) > 1) {
+            std::cout << vec[i] << ": ";
+
+            std::pair<Iterc, Iterc> p = dict.equal_range(vec[i]);
+
+            int num_in_bucket = 0;
+            while (p.first != p.second) {
+                std::cerr << p.first->second << " ";
+                ++p.first;
+                ++num_in_bucket;
+            }
+            std::cerr << "\n";
+
+            if (num_in_bucket > max_num_in_bucket) {
+                max_num_in_bucket = num_in_bucket;
+            }
+        }
+    }
+
+    ASSERT(max_num_in_bucket < 3,
+        << "I think the implementation is wrong, too many collisions.");
+
+    std::cerr << __FUNCTION__ << "() passed.\n";
+}
