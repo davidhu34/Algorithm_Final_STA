836238e68fc40edf6a8c223905589c056c1e9488
dianhenglau <dianhenglau@gmail.com>
2016-05-25 10:31:54 +0800

    Delete writer.cpp and writer.h.

--- src/util/writer.cpp
+++
@@ -1,8 +0,0 @@
-#include "util/writer.h"
-
-int Util::Writer::write(const std::vector<Cir::Path>&     paths,
-                        const std::vector<Cir::InputVec>& input_vecs,
-                        const std::string&                outfile) {
-	return 0;
-    // TODO
-}
