de55e5ca0fd74f1a1190490805f166ae37107ba6
dianhenglau <dianhenglau@gmail.com>
2016-05-26 16:31:40 +0800

    Test HashMap completed.

--- src/util/hash_map.h
+++ src/util/hash_map.h
@@ -24,6 +24,10 @@ namespace Util {
 // }
 // ```
 //
+// You can use `Util::prime_gt(n)` to get a prime number greater
+// than `n`, and use that prime number as bucket size to rehash.
+// Prime number can help hash key to distribute more evenly.
+//
 template <typename Key, typename Value, typename Hash>
 struct HashMap {
 
@@ -33,16 +37,15 @@ struct HashMap {
         Value value;
 
         // Constructor
-        explicit Pair(const Key& _key): key(_key) {}
-
         Pair(const Key& _key, const Value& _value):
-            key(_key),
+            key  (_key),
             value(_value) {}
     };
 
     // Data Member
     std::vector< std::vector<Pair> > bucket;
     Hash                             hash;
+    size_t                           size;
 
     // Constructor. Bucket size is initialized to 1.
     //
@@ -50,11 +53,14 @@ struct HashMap {
     //
     // - `hash`: A hash function for key.
     //
-    explicit HashMap(Hash _hash): bucket(1), hash(_hash) { }
+    explicit HashMap(Hash _hash): bucket(1), hash(_hash), size(0) { }
 
     // Member functions
 
-    // Insert a key-value pair.
+    // Insert a key-value pair. It won't check whether HashMap contain
+    // `key`. It just hash that key and insert that key-value pair into
+    // bucket directly. If you want to check whether HashMap contain
+    // `key` before insert, use `find_or_insert()`.
     //
     // #### Input
     //
@@ -64,6 +70,7 @@ struct HashMap {
     void insert(const Key& key, const Value& value) {
         size_t idx = hash(key) % bucket.size();
         bucket[idx].push_back((Pair(key, value)));
+        ++size;
     }
 
     // Remove a key-value pair.
@@ -76,11 +83,12 @@ struct HashMap {
     //
     // - `true` if key is found and removed, `false` otherwise
     //
-    bool erase(const Key& key) {
+    bool remove(const Key& key) {
         size_t idx = hash(key) % bucket.size();
         for (size_t i = 0; i < bucket[idx].size(); ++i) {
             if (key == bucket[idx][i].key) {
                 bucket[idx].erase(bucket[idx].begin() + i);
+                --size;
                 return true;
             }
         }
@@ -115,6 +123,32 @@ struct HashMap {
             }
         }
         assert(false && "Error: Can't find key.");
+    }
+
+    // Try to find value with key and return it. If key cannot be found,
+    // insert that key with value into HashMap.
+    //
+    // #### Input
+    //
+    // - `key`:
+    // - `value`:
+    //
+    // #### Output
+    //
+    // - Reference to value.
+    //
+    Value& find_or_insert(const Key& key, const Value& value = Value()) {
+        size_t       idx   = hash(key) % bucket.size();
+        const size_t bsize = bucket[idx].size();
+
+        for (size_t i = 0; i < bsize; ++i) {
+            if (key == bucket[idx][i].key) {
+                return bucket[idx][i].value;
+            }
+        }
+        bucket[idx].push_back((Pair(key, value)));
+        ++size;
+        return bucket[idx][bsize].value;
     }
 
     // Check whether HashMap contains this key.
--- test/src/test_util_hash_map.cpp
+++ test/src/test_util_hash_map.cpp
@@ -1,12 +1,66 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 #include "test/src/util/util.h"
-
+#include "util/hasher.h"
+#include "util/prime.h"
 #include "util/hash_map.h"
 
 void test_hash_map(void) {
     std::cerr << __FUNCTION__ << "():\n";
 
+    const char* filename = "test/cases/words.txt";
+    std::ifstream fin(filename);
+    ASSERT(fin.good(), << "Cannot open file \"" << filename << "\"\n");
+
+    typedef uint32_t (*Hasher)(const std::string&);
+    typedef std::string                     Str;
+    typedef Util::HashMap<Str, int, Hasher> HashMap;
+
+    HashMap          dict(Util::hash_str);
+    std::vector<Str> words;
+    Str              word;
+    
+    while (fin >> word) {
+        words.push_back(word);
+        dict.insert(word, 13);
+        ASSERT(dict.size == words.size(), );
+    }
+
+    dict.rehash(Util::prime_gt(dict.size));
+
+    for (size_t i = 0; i < words.size(); ++i) {
+        dict[words[i]] = 30;
+    }
+
+    const HashMap& dictref = dict;
+
+    for (size_t i = 0; i < words.size(); ++i) {
+        ASSERT(dictref.contains(words[i]),
+            << "words[" << i << "] = " << words[i] << "\n");
+    }
+    ASSERT(!dictref.contains("dianhenglau"),
+        << "What? dictref contain my name???\n");
+    
+    dict.find_or_insert("dianhenglau");
+
+    ASSERT(dictref.contains("dianhenglau"),
+        << "dict should contain my name.\n");
+
+    ASSERT(dict.size == words.size() + 1, );
+
+    dict.find_or_insert("love");
+
+    ASSERT(dict.size == words.size() + 1, );
+    
+    for (size_t i = 0; i < words.size(); ++i) {
+        dict.find_or_insert(words[i]) = 23;
+        dict.remove(words[i]);
+    }
+
+    ASSERT(dict.size == 1, );
 
     std::cerr << __FUNCTION__ << "() passed.\n";
 }
