fc015c6fd839b41403006d29673bdf528fac01f9
dianhenglau <dianhenglau@gmail.com>
2016-06-01 23:05:34 +0800

    Add constraints to parameters.

--- src/ana/analyzer.h
+++ src/ana/analyzer.h
@@ -16,10 +16,12 @@ namespace Ana {
 //
 // - `paths`     : Found sensitizable paths.
 // - `input_vecs`: Corresponding input vector that cause a path become
-//                 true path.
+//                 true path. Path start from PO, end at PI.
 // - Return an error code: 0 means successful; 1 means error encountered.
 //
 int find_sensitizable_paths(Cir::Circuit&               circuit,
+                            int                         time_constraint,
+                            int                         slack_constraint,
                             std::vector<Cir::Path>&     paths,
                             std::vector<Cir::InputVec>& input_vecs);
 
