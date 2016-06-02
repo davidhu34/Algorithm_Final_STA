2495d2850f7d912b817c69f714153ed24e49a97d
dianhenglau <dianhenglau@gmail.com>
2016-06-02 14:12:21 +0800

    Define interface.

--- src/cir/writer.cpp
+++ src/cir/writer.cpp
@@ -1,8 +1,15 @@
 #include "sta/src/cir/writer.h"
 
-int Sta::Cir::write(const std::vector<Path>&     paths,
-                    const std::vector<InputVec>& input_vecs,
-                    const std::string&           outfile)   {
+int Sta::Cir::write(
+    const Circuit&                          circuit,
+    int                                     time_constraint,
+    int                                     slack_constraint,
+    const std::vector<Path>&                paths,
+    const std::vector< std::vector<bool> >& values,
+    const std::vector<InputVec>&            input_vecs,
+    const std::string&                      outfile          ) {
+
+
 
     // TODO
     return 0;
--- src/cir/writer.h
+++ src/cir/writer.h
@@ -26,9 +26,13 @@ namespace Cir {
 //     - 1: Cannot open file.
 //     - 2: Other.
 //
-int write(const std::vector<Path>&     paths,
-          const std::vector<InputVec>& input_vecs,
-          const std::string&           outfile = "");
+int write(const Circuit&                          circuit,
+          int                                     time_constraint,
+          int                                     slack_constraint,
+          const std::vector<Path>&                paths,
+          const std::vector< std::vector<bool> >& values,
+          const std::vector<InputVec>&            input_vecs,
+          const std::string&                      outfile = "");
 
 } // namespace Cir
 } // namespace Sta
