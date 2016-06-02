17d300a2d15e5bee4960720b0be92c7c6912f290
dianhenglau <dianhenglau@gmail.com>
2016-06-02 21:08:05 +0800

    Some minor tweak.

--- src/cir/writer.cpp
+++ src/cir/writer.cpp
@@ -9,11 +9,11 @@
 static const char* hline = // Horizontal line
     "------------------------------------------------------------\n";
 static const int pin_fw     = 10; // field width
-static const int type_fw    = 40;
-static const int incr_fw    = 5;
+static const int type_fw    = 25;
+static const int incr_fw    = 10;
 static const int delay_fw   = 10;
 static const int value_fw   = 5;
-static const int timelbl_w  = 30;
+static const int timelbl_w  = 20;
 static const int timeval_w  = 10;
 
 // Print two gates.
@@ -39,7 +39,7 @@ static void print_connection(int                      delay,
     }
 
     out << setw(pin_fw)   << sout.str()
-        << setw(type_fw)  << "(" << cir.modules[g1->module].name << ")"
+        << setw(type_fw)  << "(" + cir.modules[g1->module].name + ")"
         << setw(incr_fw)  << (g1->module == Module::PI ? 0 : 1)
         << setw(delay_fw) << delay
         << setw(value_fw) << (g1_val ? "r" : "f") << "\n";
@@ -62,7 +62,7 @@ static void print_connection(int                      delay,
     }
 
     out << setw(pin_fw)   << sout.str()
-        << setw(type_fw)  << "(" << cir.modules[g2->module].name << ")"
+        << setw(type_fw)  << "(" + cir.modules[g2->module].name + ")"
         << setw(incr_fw)  << 0
         << setw(delay_fw) << delay
         << setw(value_fw) << (g1_val ? "r" : "f") << "\n";
@@ -86,7 +86,7 @@ int Sta::Cir::write(
         buf = std::cout.rdbuf();
     }
     else {
-        fout.open(outfile);
+        fout.open(outfile.c_str());
         if (!fout.good()) {
             std::cerr << "Error: Cannot open file " << outfile << ".\n";
             return 1;
@@ -104,7 +104,7 @@ int Sta::Cir::write(
         << "Benchmark { " << cir.name << " }\n\n";
 
     for (size_t i = 0; i < paths.size(); ++i) {
-        out << "Path { " << i + 1 << " }\n\n"
+        out << "Path { " << i + 1 << " }\n\n";
 
         // Print path
         out << "A True Path List\n"
@@ -114,7 +114,7 @@ int Sta::Cir::write(
             << std::setw(type_fw)  << "Type"
             << std::setw(incr_fw)  << "Incr"
             << std::setw(delay_fw) << "Path Delay"
-            << std::setw(value_fw) << "" << "\n";
+            << std::setw(value_fw) << "" << "\n"
             << hline;
 
         for (size_t j = paths[i].size(); j > 1; --j) {
@@ -130,14 +130,14 @@ int Sta::Cir::write(
         int delay = paths[i].size() - 2;
 
         out << hline
-            << std::setw(timelbl_w) << "Data Required Time"
-            << std::setw(timeval_w) << time_constraint << "\n"
-            << std::setw(timelbl_w) << "Data Arrival Time"
-            << std::setw(timeval_w) << delay << "\n"
+            << std::setw(timelbl_w) << "Data Required Time" << std::right
+            << std::setw(timeval_w) << time_constraint << "\n" << std::left
+            << std::setw(timelbl_w) << "Data Arrival Time" << std::right
+            << std::setw(timeval_w) << delay << "\n" << std::left
             << hline
-            << std::setw(timelbl_w) << "Slack"
+            << std::setw(timelbl_w) << "Slack" << std::right
             << std::setw(timeval_w) << time_constraint - delay << "\n"
-            << "}\n\n";
+            << "}\n\n" << std::left;
         
         // Print input vector
         out << "Input Vector\n"
@@ -148,17 +148,18 @@ int Sta::Cir::write(
                 << std::setw(2)  << "="
                 << std::setw(15);
 
-            if (paths[i].back() == cir.primary_inputs[j]) {
-                out << input_vecs[i][j];
+            if (paths[i].back() != cir.primary_inputs[j]) {
+                out << static_cast<int>(input_vecs[i][j]);
             }
             else {
                 out << (input_vecs[i][j] ? "r" : "f");
             }
-            out << "\n"
+            out << "\n";
         }
 
         out << "}\n\n";
     }
+    out << std::right;
 
     return 0;
 }
