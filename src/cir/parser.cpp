64994318a6759df6c7c978ab93788f94b1a424a1
dianhenglau <dianhenglau@gmail.com>
2016-06-02 23:06:49 +0800

    Add parser empty file.

---
+++ src/cir/parser.cpp
@@ -0,0 +1,9 @@
+#include "sta/src/cir/parser.h"
+
+namespace Sta {
+namespace Cir {
+
+// put function definition here
+
+} // namespace Cir
+} // namespace Sta
---
+++ src/cir/parser.h
@@ -0,0 +1,17 @@
+#ifndef STA_CIR_PARSER_H
+#define STA_CIR_PARSER_H
+
+#include <iostream>
+
+#include "sta/src/cir/components.h"
+#include "sta/src/cir/circuit.h"
+
+namespace Sta {
+namespace Cir {
+
+// put function prototype here.
+
+} // namespace Sta
+} // namespace Cir
+
+#endif // STA_CIR_PARSER_H
